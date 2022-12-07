8b2a0b03c386ee46d5d9f13b3d299c3b3a356701
EdisonPadilla <geovanny.esfot@outlook.com>
2022-12-07 13:07:22 -0500

    slider functionality

--- functionality/functionality.js
+++ functionality/functionality.js
@@ -1,21 +1,37 @@
 "use strict";
 
 const d = document;
-const menu_button = d.querySelector(".ham-btn");
-const ham_icon = d.querySelector(".ham-icon");
-const close_icon = d.querySelector(".close-icon");
+const $menu_button = d.querySelector(".ham-btn");
+const $ham_icon = d.querySelector(".ham-icon");
+const $close_icon = d.querySelector(".close-icon");
 const menu = d.querySelector(".nav-menu");
-const menu_background = d.querySelector(".menu-box-background");
+const $menu_background = d.querySelector(".menu-box-background");
+const $testimonial_articles_box = d.querySelector(".testimonial-articles-box");
+const $slider_buttons = d.querySelectorAll(".slider-btn");
+const $slider_offset_value = 105;
 
-console.log(menu_button);
-console.log(ham_icon);
-console.log(close_icon);
+console.log($menu_button);
+console.log($ham_icon);
+console.log($close_icon);
 console.log(menu);
-console.log(menu_background);
+console.log($menu_background);
+console.log($testimonial_articles_box);
+console.log($slider_buttons);
 
-menu_button.addEventListener("click", () => {
-  ham_icon.classList.toggle("clicked");
-  close_icon.classList.toggle("clicked");
+$menu_button.addEventListener("click", () => {
+  $ham_icon.classList.toggle("clicked");
+  $close_icon.classList.toggle("clicked");
   menu.classList.toggle("clicked");
-  menu_background.classList.toggle("clicked");
+  $menu_background.classList.toggle("clicked");
 });
+
+$slider_buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    let offsetValue = $slider_offset_value * index;
+    Array.from($slider_buttons).map((btn) => btn.classList.remove("selected"));
+    button.classList.add("selected");
+    $testimonial_articles_box.style.transform = `translateX(-${offsetValue}%)`;
+  });
+});
+
+$slider_buttons[0].classList.add("selected");
